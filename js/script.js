057259d98cb2ea3e451f5fa23ebdf625b1e5fa9b
Nina <mysha76@mail.ru>
2020-10-10 13:29:05 +0300

    Сделана анимация главной страницы

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const cartButton = document.querySelector('#cart-button'),
   modal = document.querySelector('.modal'),
   closeBtn = document.querySelector('.close');
 
-cartButton.addEventListener('click', event => {
+cartButton.addEventListener('click', () => {
   modal.classList.add('is-open');
 });
 
-closeBtn.addEventListener('click', event => {
+closeBtn.addEventListener('click', () => {
   modal.classList.remove('is-open');
 });
 
